309f5b0158f786e032883df633f2107ae5d7c861
kimyega <kimyega@naver.com>
2025-03-14 14:20:50 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,24 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("Hello!! My Second Java Coding!!");
+        System.out.println("Hello!! My Third Java Coding!!");
+        System.out.println("Hello!! My Fourth Java Coding!!");
+        System.out.println("Hello!! My Fifth Java Coding!!");
+        System.out.println("Hello!! My Sixth Java Coding!!");
+        System.out.println("Hello!! My Seventh Java Coding!!");
+        System.out.println("Hello!! My Eighth Java Coding!!");
+        System.out.println("Hello!! My Ninth Java Coding!!");
+        System.out.println("Hello!! My Tenth Java Coding!!");
+        System.out.println("Hello!! My Eleventh Java Coding!!");
+        System.out.println("Hello!! My Twelfth Java Coding!!");
+        System.out.println("Hello!! My Thirteenth Java Coding!!");
+        System.out.println("Hello!! My Fourteenth Java Coding!!");
+        System.out.println("Hello!! My Fifteenth Java Coding!!");
+        System.out.println("Hello!! My Sixteenth Java Coding!!");
+        System.out.println("Hello!! My Seventeenth Java Coding!!");
+        System.out.println("Hello!! My Eighteenth Java Coding!!");
+        System.out.println("Hello!! My Nineteenth Java Coding!!");
+    }
+
+}
