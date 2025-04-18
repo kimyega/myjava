d05b06011447e34b74cad2e26c93b6c3116b02ba
kimyega <kimyega@naver.com>
2025-04-18 18:03:33 +0900

    Java

--- src/main/java/mid_exam/Exam02.java
+++ src/main/java/mid_exam/Exam02.java
@@ -4,7 +4,7 @@ public class Exam02 {
     public static void main(String[] args) {
         String log = "127.0.0.1 - frank [10/Oct/2000:13:55:36-0700] \"GET /apache.pb.gif HTTP/1.0\" 200";
         int idx = log.indexOf("\"");
-        int stIdx = log.indexOf(" ", idx) + 2;
+        int stIdx = log.indexOf("/", idx);
         int endIdx = log.indexOf(" ", stIdx);
         String st = log.substring(stIdx, endIdx);
         System.out.println(st);
