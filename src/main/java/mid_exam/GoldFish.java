ceab9b2768361ef7e8bcbf9ea1390e894f9d1e39
kimyega <kimyega@naver.com>
2025-05-02 12:34:38 +0900

    Java

--- src/main/java/mid_exam/GoldFish.java
+++ src/main/java/mid_exam/GoldFish.java
@@ -5,5 +5,5 @@ public class GoldFish extends Fish {        // 부모 클래스가 Fish 인 자�
     @Override           // @Override 로 부모클래스 Fish 의 추상메서드인 swim() 을 불러와 자식클래스에서 재정의한다.
     public void swim() {
         System.out.println("헤엄치고 있습니다.");
-    }       // 헤엄치고 있습니다. 를 출력한다.
+    }       // "헤엄치고 있습니다." 를 출력한다.
 }
